6e614cd904b66018cb842ba749bd77fa31b1ae6a
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2020-12-14 08:51:03 -0300

    WIP

--- src/log.h
+++ src/log.h
@@ -6,11 +6,15 @@
 #include <stdio.h>
 
 #ifdef _DEBUG
-#define LOG(msg) printf(msg);
+#define LOG(msg) fprintf(stdout, "%s\n", msg)
 #define LOGF(fmt, ...) fprintf(stdout, fmt, __VA_ARGS__)
+#define ERR(msg) fprintf(stderr, "%s\n", msg)
+#define ERRF(fmt, ...) fprintf(stderr, fmt, __VA_ARGS__)
 #else
 #define LOG(fmt)
 #define LOGF(fmt, ...)
+#define ERR(msg)
+#define ERRF(fmt, ...)
 #endif
 
 #endif // __LOG_H__
